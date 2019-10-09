74a04626d52388656896cbf31970809fdaf16197
erriveros <erriveros@miuandes.cl>
2019-10-09 04:12:36 -0300

    merge

--- Assets/Codes/Login.cs
+++ Assets/Codes/Login.cs
@@ -10,6 +10,7 @@ using Firebase.Unity.Editor;
 public class Login : MonoBehaviour
 {
     public DatabaseReference reference;
+
     private void Start()
     {
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://class-wars.firebaseio.com/.json");
@@ -21,7 +22,6 @@ public class Login : MonoBehaviour
 
     public static string PlayerUsername;
     public static string PlayerPassword;
-    public static string PlayerEmail = null;
 
     public void OnSubmitLoginButton()
     {
@@ -55,3 +55,4 @@ public class Login : MonoBehaviour
         });
     }
 }
+
