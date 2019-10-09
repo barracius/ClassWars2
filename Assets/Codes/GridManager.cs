290ab583ada798c6f1fde899228889ad6890b7c5
beannama <ben.nar.95@gmail.com>
2019-10-09 01:00:04 -0300

    maptile creation wait

---
+++ Assets/Codes/GridManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private int rows = 2;
+    [SerializeField]
+    private int cols = 2;
+    [SerializeField]
+    private float tileSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GenerateGrid()
+    {
+
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("grasstile"));
+
+        for (int row = 0; row < rows; rows++)
+        {
+            for (int col = 0; col < cols; cols++)
+            {
+                GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+
+        Destroy(referenceTile);
+
+        float gridW = cols * tileSize;
+        float gridH = rows * tileSize;
+        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+
+    }
+}
