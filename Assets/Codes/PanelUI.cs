09ecc45721f8dd89d0ac2d718a6894a871ce5816
beannama <ben.nar.95@gmail.com>
2019-10-11 11:45:28 -0300

    UI Created, events needed

--- Assets/Codes/PanelUI.cs
+++ Assets/Codes/PanelUI.cs
@@ -5,50 +5,79 @@ using UnityEngine;
 public class PanelUI : MonoBehaviour
 {
 
-    public GameObject BotHuir;
-    public GameObject BotAtacar;
-    public GameObject BotUsar;
-    public GameObject BotP1;
-    public GameObject BotP2;
-    public GameObject BotP3;
-    public GameObject Volver;
+
+    public GameObject First_Panel;
+
+    public GameObject First_FleeBtn;
+    public GameObject First_AtkBtn;
+    public GameObject First_ItemBtn;
+    public GameObject First_SkillBtn;
+
+
+
+    public GameObject Skill_Panel;
+    public GameObject Skill_Btn1;
+    public GameObject Skill_Btn2;
+    public GameObject Skill_Btn3;
+    public GameObject Skill_BackBtn;
+
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        BotP1.SetActive(false);
-        BotP2.SetActive(false);
-        BotP3.SetActive(false);
-        Volver.SetActive(false);
+        First_Panel.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleVisibility();
     }
 
     public void Attack()
     {
-        BotHuir.SetActive(false);
-        BotUsar.SetActive(false);
-        BotAtacar.SetActive(false);
-        BotP1.SetActive(true);
-        BotP2.SetActive(true);
-        BotP3.SetActive(true);
-        Volver.SetActive(true);
+        First_Panel.SetActive(true);
+        Skill_Panel.SetActive(false);
     }
 
     public void Atras()
     {
-        BotHuir.SetActive(true);
-        BotUsar.SetActive(true);
-        BotAtacar.SetActive(true);
-        BotP1.SetActive(false);
-        BotP2.SetActive(false);
-        BotP3.SetActive(false);
-        Volver.SetActive(false);
+
+    }
+
+    public void HandleVisibility()
+    {
+        if (First_Panel.activeSelf)
+        {
+            First_FleeBtn.SetActive(true);
+            First_AtkBtn.SetActive(true);
+            First_ItemBtn.SetActive(true);
+            First_SkillBtn.SetActive(true);
+
+            Skill_Panel.SetActive(false);
+            Skill_Btn1.SetActive(false);
+            Skill_Btn2.SetActive(false);
+            Skill_Btn3.SetActive(false);
+            Skill_BackBtn.SetActive(false);
+        }
+        else if (Skill_Panel)
+        {
+            Skill_Btn1.SetActive(true);
+            Skill_Btn2.SetActive(true);
+            Skill_Btn3.SetActive(true);
+            Skill_BackBtn.SetActive(true);
+
+            Skill_Panel.SetActive(false);
+            First_FleeBtn.SetActive(false);
+            First_AtkBtn.SetActive(false);
+            First_ItemBtn.SetActive(false);
+            First_SkillBtn.SetActive(false);
+
+
+        }
     }
 }
