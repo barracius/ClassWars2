24868af77acfb7cb813a56695688229b58909d2d
beannama <ben.nar.95@gmail.com>
2019-10-11 12:39:37 -0300

    UI Updated

--- Assets/Codes/PanelUI.cs
+++ Assets/Codes/PanelUI.cs
@@ -13,13 +13,17 @@ public class PanelUI : MonoBehaviour
     public GameObject First_ItemBtn;
     public GameObject First_SkillBtn;
 
-
-
     public GameObject Skill_Panel;
     public GameObject Skill_Btn1;
     public GameObject Skill_Btn2;
     public GameObject Skill_Btn3;
     public GameObject Skill_BackBtn;
+
+    public GameObject Atk_Panel;
+    public GameObject Atk_Btn1;
+    public GameObject Atk_Btn2;
+    public GameObject Atk_Btn3;
+    public GameObject Atk_BackBtn;
 
 
 
@@ -29,7 +33,7 @@ public class PanelUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        First_Panel.SetActive(true);
+        HandleVisibilityFirst(true);
     }
 
     // Update is called once per frame
@@ -40,45 +44,80 @@ public class PanelUI : MonoBehaviour
 
     public void Skill()
     {
+
         First_Panel.SetActive(false);
+        Atk_Panel.SetActive(false);
         Skill_Panel.SetActive(true);
     }
-
-    public void Atras()
+    public void Atk()
     {
-        First_Panel.SetActive(true);
+
+        First_Panel.SetActive(false);
         Skill_Panel.SetActive(false);
+        Atk_Panel.SetActive(true);
     }
+
+    public void Back_to_First()
+    {
+
+        Atk_Panel.SetActive(false);
+        Skill_Panel.SetActive(false);
+        First_Panel.SetActive(true);
+    }
+
 
     public void HandleVisibility()
     {
         if (First_Panel.activeSelf)
         {
-            First_FleeBtn.SetActive(true);
-            First_AtkBtn.SetActive(true);
-            First_ItemBtn.SetActive(true);
-            First_SkillBtn.SetActive(true);
+            HandleVisibilityFirst(true);
 
-            Skill_Panel.SetActive(false);
-            Skill_Btn1.SetActive(false);
-            Skill_Btn2.SetActive(false);
-            Skill_Btn3.SetActive(false);
-            Skill_BackBtn.SetActive(false);
+            HandleVisibilitySkill(false);
+            HandleVisibilityAtk(false);
         }
         else if (Skill_Panel.activeSelf)
         {
-            Skill_Btn1.SetActive(true);
-            Skill_Btn2.SetActive(true);
-            Skill_Btn3.SetActive(true);
-            Skill_BackBtn.SetActive(true);
 
-            First_Panel.SetActive(false);
-            First_FleeBtn.SetActive(false);
-            First_AtkBtn.SetActive(false);
-            First_ItemBtn.SetActive(false);
-            First_SkillBtn.SetActive(false);
+            HandleVisibilitySkill(true);
 
+            HandleVisibilityFirst(false);
+            HandleVisibilityAtk(false);
 
         }
+        else if (Atk_Panel.activeSelf)
+        {
+            HandleVisibilityAtk(true);
+
+            HandleVisibilityFirst(false);
+            HandleVisibilitySkill(false);
+
+        }
+    }
+
+    void HandleVisibilityFirst(bool boolean)
+    {
+        First_Panel.SetActive(boolean);
+        First_FleeBtn.SetActive(boolean);
+        First_AtkBtn.SetActive(boolean);
+        First_ItemBtn.SetActive(boolean);
+        First_SkillBtn.SetActive(boolean);
+    }
+
+    void HandleVisibilitySkill(bool boolean)
+    {
+        Skill_Panel.SetActive(boolean);
+        Skill_Btn1.SetActive(boolean);
+        Skill_Btn2.SetActive(boolean);
+        Skill_Btn3.SetActive(boolean);
+        Skill_BackBtn.SetActive(boolean);
+    }
+
+    void HandleVisibilityAtk(bool boolean)
+    {
+        Atk_Panel.SetActive(boolean);
+        Atk_Btn1.SetActive(boolean);
+        Atk_Btn2.SetActive(boolean);
+        Atk_Btn3.SetActive(boolean);
+        Atk_BackBtn.SetActive(boolean);
     }
 }
