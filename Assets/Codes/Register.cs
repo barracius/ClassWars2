5ec2ec0a03c8f58af2f737472c992a7e657c1951
beannama <ben.nar.95@gmail.com>
2019-10-11 02:18:34 -0300

    restored files

---
+++ Assets/Codes/Register.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Database;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class Register : MonoBehaviour
+{
+    public DatabaseReference reference;
+    private Firebase.Auth.FirebaseAuth auth;
+    private Firebase.Auth.FirebaseUser newUser;
+
+    private void awake()
+    {
+
+    }
+    private void Start()
+    {
+
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://class-wars.firebaseio.com/.json");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public InputField RegisterUsernameText;
+    public InputField RegisterPasswordText;
+    public InputField RegisterEmailText;
+
+    public static string PlayerUsername;
+    public static string PlayerPassword;
+    public static string PlayerEmail = null;
+
+    public void OnSubmitRegisterButton()
+    {
+        PlayerUsername = RegisterUsernameText.text;
+        PlayerPassword = RegisterPasswordText.text;
+        PlayerEmail = RegisterEmailText.text;
+        PostToDatabase();
+    }
+    private void PostToDatabase()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+        auth.CreateUserWithEmailAndPasswordAsync(PlayerEmail, PlayerPassword).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+            }
+
+            //Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})"
+                    , newUser.DisplayName, newUser.UserId);
+        });
+
+        Firebase.Auth.Credential credential = Firebase.Auth.EmailAuthProvider.GetCredential(PlayerEmail, PlayerPassword);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+            if (newUser != null)
+            {
+                Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile
+                {
+                    DisplayName = PlayerUsername
+                };
+                newUser.UpdateUserProfileAsync(profile).ContinueWith(tarea =>
+                {
+                    if (tarea.IsCanceled)
+                    {
+                        Debug.LogError("UpdateUserProfileAsync was canceled.");
+                        return;
+                    }
+
+                    if (tarea.IsFaulted)
+                    {
+                        Debug.LogError("UpdateUserProfileAsync encountered an error: " + tarea.Exception);
+                        return;
+                    }
+
+                    Debug.Log("User profile updated successfully.");
+                    Debug.Log(newUser.DisplayName);
+                });
+            }
+        });
+    }
+
+}
