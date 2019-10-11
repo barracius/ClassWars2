cfcab58f7713fa4019db82e9c99f63cf84ed66d0
erriveros <erriveros@miuandes.cl>
2019-10-11 00:58:16 -0300

    tilemap and user movement in map worcking

---
+++ Assets/Codes/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // What is the player doing with the controls?
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 0);
+ 
+        // Update the ships position each frame
+        transform.position += Time.deltaTime * speed * move;
+    }
+}
