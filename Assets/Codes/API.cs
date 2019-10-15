3be2cacdf658c8b9874027ca80d8455e9225668c
beannama <ben.nar.95@gmail.com>
2019-10-15 13:14:34 -0300

    switch branch

---
+++ Assets/Codes/API.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Text;
+using System;
+
+
+public class API : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void startCalling()
+    {
+        StartCoroutine(GetRequest("https://afternoon-spire-83789.herokuapp.com/books"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                text.text = webRequest.downloadHandler.text;
+            }
+        }
+    }
+}
