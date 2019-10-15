69538cdf49b2d4856b34e73a2bf7375b21254b05
erriveros <erriveros@miuandes.cl>
2019-10-14 22:04:52 -0300

    avance gigante

---
+++ Assets/Codes/EventScene.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventScene : MonoBehaviour
+{
+    public FloatValue currentHealth;
+    public Signal playerHealthSignal;
+    
+    public GameObject enemy;
+    public GameObject exlamation;
+    public GameObject search;
+    public GameObject move;
+    public GameObject skill;
+    public GameObject rest;
+    public GameObject dialogBox;
+
+    public Text dialogText;
+
+    public string dialog;
+
+    public bool dialogActive;
+
+    public bool inBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (dialogActive)
+            {
+                
+                turnOnMenu();
+                dialogActive = false;
+                dialogBox.SetActive(false);
+                if (enemy)
+                {
+                    exlamation.SetActive(false);
+                    enemy.SetActive(true);
+                    if (currentHealth.initialValue > 0)
+                    {
+                        currentHealth.initialValue -= 1;
+                        playerHealthSignal.Raise();
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            Debug.Log(inBox);
+            inBox = true;
+            Debug.Log(inBox);
+            Debug.Log("player in scene");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inBox = false;
+            Debug.Log("player out scene");
+        }
+    }
+
+    public void Search()
+    {
+        Debug.Log(inBox);
+        
+        if (inBox)
+        {
+            if (enemy)
+            {
+                exlamation.SetActive(true);
+            }
+            turnOffMenu();
+            Debug.Log("coming in");
+            dialogActive = true;
+            dialogBox.SetActive(true);
+            dialogText.text = dialog;
+            dialog = "nothing";
+        }
+    }
+    
+    public void turnOffMenu()
+    {
+        rest.SetActive(false);
+        move.SetActive(false);
+        skill.SetActive(false);
+        search.SetActive(false);
+    }
+
+    public void turnOnMenu()
+    {
+        rest.SetActive(true);
+        move.SetActive(true);
+        skill.SetActive(true);
+        search.SetActive(true);
+    }
+
+    public void Skill()
+    {
+        Debug.Log("skill");
+    }
+}
--- Assets/Codes/PlayerMovement.cs
+++ Assets/Codes/PlayerMovement.cs
@@ -4,21 +4,166 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed = 10f;
+    private bool movx = false;
+    private bool movy = false;
+    private int xChange;
+    private int yChange;
+
+    public GameObject up;
+    public GameObject down;
+    public GameObject right;
+    public GameObject left;
+    public GameObject menu;
+    
+    
+    private int c = 312;
+    private int y = 177;
+    public float speed;
+    public Vector2 cameraChange;
+    private CameraMovement cam;
+    
+    
+    private Rigidbody2D myRigidbody;
+
+    private Vector3 change;
+
+    private Animator animator;
+
+    public float moveX;
+
+    public float moveY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main.GetComponent<CameraMovement>();
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // What is the player doing with the controls?
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical"), 0);
- 
-        // Update the ships position each frame
-        transform.position += Time.deltaTime * speed * move;
+        if (movx)
+        {
+            if (c == 0)
+            {
+                movx = false;
+                TurnOnMoveButtons();
+                c = 312;
+                
+            }
+
+            if (c == 170)
+            {
+                cam.minPosition += cameraChange;
+                cam.maxPosition += cameraChange;
+            }
+            change = Vector2.zero;
+            change.x = xChange;
+            change.y = yChange;
+            UpdateAnimationAndMove();
+            c -= 1;
+        }
+        if (movy)
+        {
+            if (y == 0)
+            {
+                movy = false;
+                TurnOnMoveButtons();
+                y = 177;
+                
+            }
+
+            if (y == 75)
+            {
+                cam.minPosition += cameraChange;
+                cam.maxPosition += cameraChange;
+            }
+            change = Vector2.zero;
+            change.x = xChange;
+            change.y = yChange;
+            UpdateAnimationAndMove();
+            y -= 1;
+        }
+        change = Vector2.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    public void moveLeft()
+    {
+        TurnOffMoveButtons();
+        xChange = -1;
+        yChange = 0;
+        cameraChange.x = -18;
+        cameraChange.y = 0;
+        movx = true;
+    }
+    public void moveUo()
+    {
+        TurnOffMoveButtons();
+        cameraChange.x = 0;
+        cameraChange.y = 10;
+        xChange = 0;
+        yChange = 1;
+        movy = true;
+    }
+    public void moveDown()
+    {
+        TurnOffMoveButtons();
+        cameraChange.x = 0;
+        cameraChange.y = -10;
+        xChange = 0;
+        yChange = -1;
+        movy = true;
+    }
+    public void moveRight()
+    {
+        TurnOffMoveButtons();
+        cameraChange.x = 18;
+        cameraChange.y = 0;
+        xChange = 1;
+        yChange = 0;
+        movx = true;
+
+    }
+    public void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+        if (movx || movy)
+        {
+            animator.SetFloat("moveX", xChange);
+            animator.SetFloat("moveY", yChange);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+    public void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position  + Time.deltaTime * speed * change);
+    }
+
+    public void TurnOffMoveButtons()
+    {
+        menu.SetActive(false);
+        up.SetActive(false);
+        down.SetActive(false);
+        right.SetActive(false);
+        left.SetActive(false);
+    }
+    public void TurnOnMoveButtons()
+    {
+        menu.SetActive(true);
+        up.SetActive(true);
+        down.SetActive(true);
+        right.SetActive(true);
+        left.SetActive(true);
     }
 }
